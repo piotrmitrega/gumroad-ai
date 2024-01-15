d3fa92030a9080576e84111927cd6367077f5002
piotrmitrega <piotrmitrega1@gmail.com>
2024-01-15 23:31:22 +0100

    Make sure token is always fresh

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -7,18 +7,14 @@ class Api::SessionsController < ApplicationController
 
     Rails.logger.info "Signed in user: #{user_info.to_json}"
 
-    user = User.find_or_create_by(:gumroad_id => auth.info.id) do |u|
-      u.gumroad_id = user_info.id
-      u.gumroad_token = auth.credentials.token
-      u.email = user_info.email
-      u.name = user_info.name
-      u.profile_url = user_info.profile_url
-    end
+    user = User.find_or_initialize_by(gumroad_id: user_info.id)
+    user.gumroad_token = auth.credentials.token
+    user.email = user_info.email if user.email.blank?
+    user.name = user_info.name if user.name.blank?
+    user.profile_url = user_info.profile_url if user.profile_url.blank?
 
     SlackService.send_message("User #{user.name} (#{user.email}) has logged in")
-
     session[:user_id] = user.id
-
     redirect_to root_path
   end
 
